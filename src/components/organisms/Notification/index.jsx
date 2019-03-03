e9371ab1e56c3906ab5d0c6919d8932349b1da3e
tamanugi <tamanugi@gmail.com>
2019-03-03 19:39:42 +0900

    ✨ atoms: MediaObjectLayout

--- src/components/organisms/Notification/index.jsx
+++ src/components/organisms/Notification/index.jsx
@@ -5,18 +5,7 @@ import Heading from '../../atoms/Heading/index'
 import { InfoTxt } from '../../atoms/Txt/index'
 import Time from '../../atoms/Time/index'
 import DeleteButton from '../../molecules/DeleteButton/index'
-
-// export class NotificationContainer extends Component {
-//   constructor() {
-//     super()
-//     this.onClickDelete = ::this.onClickDelete
-//   }
-
-//   onClickDelete(...args) {
-//     const { onClickDelete, program } = this.props
-//     onClickDelete(...args, program)
-//   }
-// }
+import MediaObjectLayout from '../../atoms/MediaObjectLayout';
 
 const NotificationPresenter = ({
   program,
@@ -24,19 +13,15 @@ const NotificationPresenter = ({
   onClickDelete,
   ...props
 }) => (
-  <section className={ [ styles.root, className ].join(' ') } { ...props }>
-    <div>
-      <Img src={ program.thumbnail } className={ styles.media } width="128" height="72" />
-    </div>
-    <div className={ styles.body }>
-      <Heading level={ 3 } visualLevel={ 6 }>{ program.title }</Heading>
-      <InfoTxt size="s">{ program.channelName }</InfoTxt>
-      <InfoTxt size="s">
-        <Time format="MM月DD日(ddd)HH:mm">{ program.startAt }</Time> 〜 <Time format="MM月DD日(ddd)HH:mm">{ program.endAt }</Time>
-      </InfoTxt>
-      <DeleteButton onClick={ onClickDelete } className={ styles.del } />
-    </div>
-  </section>
+  <MediaObjectLayout className={ [ styles.root, className ].join(' ') } { ...props }>
+    <Img src={ program.thumbnail } className={ styles.media } width="128" height="72" />
+    <Heading level={ 3 } visualLevel={ 6 }>{ program.title }</Heading>
+    <InfoTxt size="s">{ program.channelName }</InfoTxt>
+    <InfoTxt size="s">
+      <Time format="MM月DD日(ddd)HH:mm">{ program.startAt }</Time> 〜 <Time format="MM月DD日(ddd)HH:mm">{ program.endAt }</Time>
+    </InfoTxt>
+    <DeleteButton onClick={ onClickDelete } className={ styles.del } />
+  </MediaObjectLayout>
 )
 
 const NotificationContainer = ({
